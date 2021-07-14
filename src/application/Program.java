a26803e88e2ef97f2ae0fca5918eac310529b933
Marcelo Goncalves <marceloluissgprofissional@gmail.com>
2021-07-14 09:02:01 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,18 @@
 package application;
 
-import model.dao.DaoFactory;
+import java.util.Date;
+
 import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		//Injeção de independencia sem incremento
-		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller = sellerDao.findById(3);
-		
-		System.out.println(seller);
-
+	
 	}
 
 }
